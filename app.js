95f06858559713683e47f5a64501c4f316afba39
Ramiro Nasello <rnasello@hotmail.com>
2022-06-17 18:38:21 -0300

    mi primer commit

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-let checkout = [];
-let total = 0;
-let condicional = "si";
+let texto=document.getElementsByClassName("texto")
+
+titulo.innerText="Tienda oficial de la F1 2022"
+
+let parrafo= document.createElement(`p`)
+
+parrafo.innerText="A continuacion se muestran los 4 productos a elegir"
+
+
+
+
 let deposito = [
   {
     sku: 1,
     name: "Gorra de Red Bull",
     price: 2500,
+    img: "./fotos/gorra.jpg"  
   },
   {
     sku: 2,
     name: "Remera de Ferrari",
     price: 8000,
+    img: "./fotos/gorra.jpg" 
   },
   {
     sku: 3,
-    name: "Pantalon de Mercedes Benz",
-    price: 10000,
+    name: "Campera de Mclaren",
+    price: 25000,
+    img:"./fotos/gorra.jpg" 
   },
   {
     sku: 4,
-    name: "Campera de Mclaren",
-    price: 25000,
+    name: "Zapatillas de Mercedes Benz",
+    price: 18500,
+    img:"./fotos/gorra.jpg" 
   },
 ];
 
+const productsContainer=document.querySelector("#products-container")
 
-// Ejecucion del programa
+deposito.forEach((element) => {
+  
+  const div = document.createElement(`div`)
 
-alert("Bienvenidos a la tienda de compras de la F1");
+  div.innerHTML= `<h3>${deposito.sku}</h3>
+                 <p> ${deposito.name}</p>
+                 <img src=${deposito.img}/>
+                 <hr>
+                 `  
+  productsContainer.append(div)
+});
 
 
-while (condicional == "si") {
-  let carrito = Number(
-    prompt(`Ingrese el articulo que desee adquirir:
-    1. Gorra de Red Bull
-    2. Remera de Ferrari
-    3. Pantalon de Mercedes Benz
-    4. Campera de Mclaren:`)
-  );
-  if (carrito < 5 && carrito > 0) {
-    checkout.push(carrito);
-    condicional = prompt(
-      "Quiere realizar otra compra? Responda a si o no"
-    ).toLowerCase();
-    if (condicional != "si" && condicional != "no") {
-      alert("No ingresaste una respuesta valida, vuelva a intentarlo");
-      condicional = prompt(
-        "Quiere realizar otra compra? Responda a si o no"
-      ).toLowerCase();
-    }
-  } else {
-    alert("Ese producto no se encuentra en stock");
-  }
-}
 
-for (let producto of checkout) {
-  total += deposito[producto - 1].price;
-}
 
-alert(`Su total es: $${total}`);
 
-alert("Gracias por tu compra");
+
+
+
+
+
+
+
+
